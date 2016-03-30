8597b2a7b7604c32628d17b30dc31a8f1573e070
Fadhil Mandaga <firodj@gmail.com>
2016-03-30 16:00:21 +0700

    register nr-custom-metric and set-category

--- newrelic.cpp
+++ newrelic.cpp
@@ -300,7 +300,8 @@ public:
         HHVM_FE(newrelic_notice_error_intern);
         HHVM_FE(newrelic_add_attribute_intern);
         HHVM_FE(newrelic_set_external_profiler);
-
+        HHVM_FE(newrelic_custom_metric);
+        HHVM_FE(newrelic_transaction_set_category);
         loadSystemlib();
     }
 
